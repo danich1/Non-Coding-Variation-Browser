d0a453d053da7731e4dadb87f3aee858fb1bdb89
David <danich1@umbc.edu>
2016-05-25 14:03:06 -0400

    Fixing my foolish merge

--- Database Insertion/production_database_inserter.py
+++ Database Insertion/production_database_inserter.py
@@ -9,14 +9,16 @@ import gzip
 
 parser = argparse.ArgumentParser(prog="CHIP Database Inserter",description="This is a database inserter that takes chip signals/snp information and converts them into json format.")
 parser.add_argument("-tr", "--track_info", help="Use this to insert track access information",nargs=1,metavar=("Track Metadata File"))
-parser.add_argument("-cp","--chip_peak", help="Use this to parse the track file",nargs='+',metavar=("<Track File>"))
+parser.add_argument("-cp","--chip_peak", help="Use this to parse the track file",action='store_true')
 parser.add_argument("-s", "--snp", help="Use this to parse snp_location files", nargs=2, metavar=("SNP_File","Trait"))
 parser.add_argument("-d", "--ld", help="Use this to parse LD files", nargs=2, metavar=("LD_File","Population"))
-parser.add_argument("-c", "--chr", help="Use this to specify a specific chromosome (chr# or chrX or chrY) if -cp or --chip_peak is not used", nargs=1, metavar=("chromosome"))
+parser.add_argument("-c", "--chr", help="Use this option to narrow the data insertion to a given chromosome (chr# or chrX or chrY). ", nargs=1, metavar=("chromosome"))
 args = parser.parse_args()
+base_file_path = "/Users/Dave/Documents/Voight_lab/CHIP-Seq/Chip_Peaks/"
 
 #set up the database connection
 conn = sqlite3.connect("db.sqlite3")
+print "WRITING TRACK INFO"
 if args.track_info:
     query = conn.cursor()
     tracker = pd.read_csv(args.track_info[0])
@@ -24,35 +26,41 @@ if args.track_info:
     labels = {item[0] for item in query.fetchall()}
     for index,file_row in tracker.iterrows():
         if os.path.splitext(file_row["File Name"])[0] not in labels:
-            query.execute("INSERT INTO nc_snp_chip_label (accession,lab,cell_type,label,replicate) VALUES ('%s','%s','%s','%s','%d')" % (os.path.splitext(file_row["File Name"])[0],file_row["Lab"],file_row["Biosample Term Name"],file_row["Target"],file_row["Replicate"] if not(pd.isnull(file_row["Replicate"])) else -1))
+            query.execute("INSERT OR IGNORE INTO nc_snp_chip_label (accession,lab,cell_type,label,replicate) VALUES ('%s','%s','%s','%s','%d')" % (os.path.splitext(file_row["File Name"])[0],file_row["Lab"],file_row["Biosample Term Name"],file_row["Target"],file_row["Replicate"] if not(pd.isnull(file_row["Replicate"])) else -1))
     query.close()
 
-if args.chip_peak:
-    query = conn.cursor()
-    start = time.time()
-    query.execute("SELECT Max(id) FROM nc_snp_chip_seq")
-    result = query.fetchall()[0]
-    for seq_file in args.chip_peak:
-        with gzip.open(seq_file,"r") as f:
-            for line in f:
+    print "WRITING CHIP DATA"
+    if args.chip_peak:
+        query = conn.cursor()
+        start = time.time()
+        query.execute("SELECT 1 FROM nc_snp_chip_seq")
+        result = query.fetchall()
+        if len(result) != 0:
+            query.execute("SELECT Max(id) FROM nc_snp_chip_seq")
+            result = query.fetchall()[0]
+        for seq_file in tracker["File Name"].values:
+            seq_file =  base_file_path + seq_file[0:seq_file.index('.')] + ".bedgraph"
+            with open(seq_file,"r") as f:
                 counter = result[0]+1 if len(result) > 0 else 1
-                data = line.split("\t")
-                chrom = data[0]
-                seq_start = data[1]
-                seq_end = data[2]
-                seq_value = data[3]
-                print "INSERT INTO nc_snp_chip_seq (id,chr,start,end,peak,label_id) VALUES ('%d','%s','%d','%d','%f','%s')" % (counter,chrom,int(seq_start),int(seq_end),float(seq_value),os.path.splitext(seq_file.split("/")[-1])[0])
-                counter = counter + 1
-    end = time.time()
-    print "Time Taken: %fs" % (end-start)
-    query.close()
+                for line in f:
+                    data = line.split("\t")
+                    chrom = data[0]
+                    seq_start = data[1]
+                    seq_end = data[2]
+                    seq_value = data[3]
+                    query.execute("INSERT OR IGNORE INTO nc_snp_chip_seq (id,chr,start,end,peak,label_id) VALUES ('%d','%s','%d','%d','%f','%s')" % (counter,chrom,int(seq_start),int(seq_end),float(seq_value),os.path.splitext(seq_file.split("/")[-1])[0]))
+                    counter = counter + 1
+        end = time.time()
+        print "Time Taken: %fs" % (end-start)
+        query.close()
 
+print "WRITING SNP DATA"
 if  args.snp:
     snp_dict = {}
     query = conn.cursor()
     #update this for each snp file
-    header_label = {"rs_number":0,"chromosome":1,"position":2,"pvalue":3}
-    query.execute("INSERT OR IGNORE INTO nc_snp_phenotype (name) VALUES ('%s')" % (args.snp[2]))
+    header_label = {"chromosome":0,"rs_number":1,"position":2,"pvalue":3}
+    query.execute("INSERT OR IGNORE INTO nc_snp_phenotype (name) VALUES ('%s')" % (args.snp[1]))
     start = time.time()
     query.execute("SELECT Max(id) FROM nc_snp_phenotypemap")
     result = query.fetchall()[0]
@@ -73,6 +81,7 @@ if  args.snp:
                 query.execute("INSERT OR IGNORE INTO nc_snp_phenotypemap (id,p_val,log_score,phenotype_id,snp_id) VALUES('%d','%.4g','%f','%s','%s') " % (row_id,data[header_label["pvalue"]],math.log10(data[header_label["pvalue"]]),args.snp[1],data[header_label["rs_number"]]))
                 row_id = row_id + 1
 
+    print "WRITING LD DATA"
     if args.ld:
         query.execute("SELECT Max(id) FROM nc_snp_r_squared")
         result = query.fetchall()[0]
@@ -85,6 +94,8 @@ if  args.snp:
                     if data[4] in snp_dict:
                         query.execute("INSERT OR IGNORE INTO nc_snp_r_squared (id,val,population,snp_A_id,snp_B_id) VALUES ('%d','%f','%s','%s','%s')" % (row_id,data[6],args.ld[1],snp_dict[data[1]],snp_dict[data[4]]))
                         row_id = row_id + 1
+    end = time.time()
+    print "Time Taken: %fs" % (end-start)
     query.close()
 
 conn.commit()
